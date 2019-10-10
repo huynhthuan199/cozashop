92712ad8be2f9439d6367c7214fcd61de1eced4c
huynh thuan <huynhthuan199@gmail.com>
2019-10-10 16:01:24 +0700

    update

--- CozaShop/src/main/java/com/cozashop/controller/admin/UserController.java
+++ CozaShop/src/main/java/com/cozashop/controller/admin/UserController.java
@@ -18,7 +18,7 @@ public class UserController {
 	@GetMapping("user")
 	public String index(ModelMap model) {
 		model.addAttribute("txtSearch", "");
-		System.out.println(123);
+		System.out.println(1234);
 		model.addAttribute("listusers", userService.findAll());
 		return "admin/user";
 	}
