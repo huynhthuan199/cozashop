d0ce8728ad422b27ec1e1dc0c2d18f8ebc3d89ad
QuoxAnk <quoxank1796>
2019-10-10 15:57:40 +0700

    update UserController

--- CozaShop/src/main/java/com/cozashop/controller/admin/UserController.java
+++ CozaShop/src/main/java/com/cozashop/controller/admin/UserController.java
@@ -18,6 +18,7 @@ public class UserController {
 	@GetMapping("user")
 	public String index(ModelMap model) {
 		model.addAttribute("txtSearch", "");
+		System.out.println(123);
 		model.addAttribute("listusers", userService.findAll());
 		return "admin/user";
 	}
